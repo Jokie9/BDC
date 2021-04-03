f1a642b1ec90e98c7c6fad13e9280eec4c37cdaf
Jokie <zanatta.gabriele97@gmail.com>
2021-04-03 15:30:07 +0200

    Completed ;)

--- src/main/java/G22HW1.java
+++ src/main/java/G22HW1.java
@@ -1,33 +1,3 @@
-/*
-ASSIGNMENT. You must develop a Spark program to identify the T best products from a review dataset of an online retailer.
-
-DATA FORMAT: a review dataset is provided as a file with one review per row. A review consists of 4 comma-separated fields: ProductID (string), UserID (string),
-Rating (integer in [1,5] represented as a real), Timestamp (integer).
-An example of review is: B00005N7P0,AH2IFH762VY5U,5.0,1005177600
-
-TASK: you must write a program GxxHW1.java (for Java users) or GxxHW1.py (for Python users), where xx is your two-digit group number, which receives in input two integers K and T,
-and path to a file storing a review dataset, and does the following things:
-
-1. Reads the input set of reviews into an RDD of strings called RawData (each review is read as a single string), and subdivides it into K partitions.
-
-2. Transform the RDD RawData into an RDD of pairs (String,Float) called normalizedRatings, so that for each string of RawData representing a review (ProductID,UserID,Rating,Timestamp),
-NormalizedRatings contains the pair (ProductID,NormRating), where NormRating=Rating-AvgRating and AvgRating is the average rating of all reviews by the user "UserID".
-To accomplish this step you can safely assume that there are a few reviews for each user. Note that normalizedRatings may contain several pairs for the same product,
-one for each existing review for that product!
-
-3. Transform the RDD normalizedRatings into an RDD of pairs (String,Float) called maxNormRatings which,
-for each ProductID contains exactly one pair (ProductID, MNR) where MNR is the maximum normalized rating of product "ProductID".
-The maximum should be computed either using the reduceByKey method or the mapPartitionsToPair/mapPartitions method. (Hint: get inspiration from the WordCountExample program).
-
-4. Print the T products with largest maximum normalized rating, one product per line. (Hint: use a combination of sortByKey and take methods.)
-To test your program you can use file input_20K.csv, which contains 20000 reviews. The output on this dataset is shown in file output_20K.txt. For your homework adopt the same output format.
-
-SUBMISSION INSTRUCTIONS. Each group must submit a single file (GxxHW1.java or GxxHW1.py depending on whether you are Java or Python users, where xx is your ID group).
-Only one student per group must submit the files in Moodle Exam using the link provided in the Homework1 section.
-Make sure that your code is free from compiling/run-time errors and that you use the file/variable names in the homework description, otherwise your score will be penalized.
-If you have questions about the assignment, contact the teaching assistants (TAs) by email to bdc-course@dei.unipd.it .
-The subject of the email must be "HW1 - Group xx", where xx is your ID group. If needed, a zoom meeting between the TAs and the group will be organized.
-*/
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -41,8 +11,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.*;
 
-// INPUT FILE: input_20K.csv
-// OUTPUT FILE: output_20K.txt
+// GROUP 22 : FEDERICA VETTOR, GABRIELE ZANATTA
 
 public class G22HW1 {
 
@@ -54,7 +23,11 @@ public class G22HW1 {
         }
 
         // SPARK SETUP
-        SparkConf conf = new SparkConf(true).setAppName("G22HW1");
+        // SETUP WITH Master set to Local machine
+        SparkConf conf = new SparkConf(true).setAppName("G22HW1").setMaster("local[*]");
+        // SETUP WITHOUT Master set to Local machine
+        //SparkConf conf = new SparkConf(true).setAppName("G22HW1");
+
         JavaSparkContext sc = new JavaSparkContext(conf);
         sc.setLogLevel("WARN");
 
@@ -68,10 +41,11 @@ public class G22HW1 {
 
         // SET GLOBAL VARIABLES
         JavaPairRDD<String, Float> normalizedRating;
+        JavaPairRDD<String, Float> maxNormRatings;
 
         //MAPREDUCE ALGORITHM
         normalizedRating = RawData
-                //R1 MAP PHASE: copy of RawData in (UserID, RATE)
+                //R1 MAP PHASE: create a Tuple2 < userID , Tuple2 < productID, rating > >
                 .flatMapToPair((document) -> {
                     String[] tokens = document.split(",");
                     // pairs -> (UserID, (ProductID, Rating))
@@ -81,21 +55,22 @@ public class G22HW1 {
                     }
                     return pairs.iterator();
                 })
-                //R1 REDUCE PHASE: compute avg for each UserID (no repetition)
+                //R1 REDUCE PHASE: return PairRDD with Tuple2< Product, normRating >
                 .groupByKey()
                 .flatMapToPair((element) -> {
 
                     ArrayList<Tuple2<String, Float>> pairs = new ArrayList<>();
                     Iterator<Tuple2<String, Float>> list = element._2().iterator();
 
+                    // sumOfRatings = sum of ratings of a specific user
                     Float sumOfRatings = 0F;
+                    // sumOfRatings = number of ratings of a specific user
                     Float numOfRatings = 0F;
                     Float avg;
 
-                    // element -> Tuple2 (UserID, Tuple2<ProductID, Rating>)
                     while (list.hasNext()){
 
-                        // tuple -> (UserID, Tuple2<ProductID, Rating>)
+                        // tuple -> Tuple2 < ProductID, Rating >
                         Tuple2<String, Float> tuple = list.next();
 
                         Float rating = tuple._2();
@@ -105,14 +80,16 @@ public class G22HW1 {
                     }
 
                     avg = sumOfRatings/numOfRatings;
+
                     list=element._2().iterator();
 
                     while (list.hasNext()){
 
-                        // tuple -> (UserID, Tuple2<ProductID, Rating>)
+                        // tuple -> Tuple2 < ProductID, Rating >
                         Tuple2<String, Float> tuple = list.next();
 
                         String productID = tuple._1();
+                        // normRating = rating of the product - average rating of the user
                         Float normRating = tuple._2() - avg;
 
                         pairs.add(new Tuple2<>(productID, normRating));
@@ -121,25 +98,54 @@ public class G22HW1 {
                    return pairs.iterator();
                 });
 
+        maxNormRatings = normalizedRating
+                //R2 MAP PHASE: propagate
+                //R2 REDUCE PHASE: return PairRDD with Tuple2< Product, maxNormRating >
+                .groupByKey()
+                .flatMapToPair((element) -> {
 
-        normalizedRating.foreach(data -> {
+                    ArrayList<Tuple2<String, Float>> pairs = new ArrayList<>();
+                    Iterator<Float> list = element._2().iterator();
 
-                System.out.println("UserID="+data._1() + " avg=" + data._2());
-        });
-        //R2 MAP PHASE: normalize (ProductID, NormRate)
+                    // max = set to -6F because minimum possible rating is -5F
+                    Float max = -6F;
+
+                    // calculate max
+                    while (list.hasNext()){
+
+                        Float f = list.next();
+                        if( f > max)
+                            max = f;
+                    }
+
+                    pairs.add(new Tuple2<>(element._1(), max));
+
+                    return pairs.iterator();
+                });
+
+        List<Tuple2<Float, String>> list;
+        list = maxNormRatings
+                //R3 MAP PHASE: switch key with value
+                .flatMapToPair( element -> {
+
+                    ArrayList<Tuple2<Float, String>> pairs = new ArrayList<>();
+
+                    pairs.add(new Tuple2<>(element._2(), element._1()));
 
 
-        //Sort pairs by value
+                    return pairs.iterator();
+                })
+                //R3 REDUCE PHASE: sort in descending order and take the first T positions
+                .sortByKey(false).take(T);
 
 
         // PRINTING THE OUTPUT: first T Product
         // FIRST LINE OF THE OUTPUT
         System.out.println("INPUT PARAMETERS: K="+K+" T="+T+" file="+args[2]+"\n");
-        // WRITE HERE THE FIRST T PRODUCT WITH THE LARGEST MAXIMUM NORMALIZED RATING (ONE PER LINE)
-
-
-
+        System.out.println("OUTPUT");
+        // PRINTING THE FIRST T PRODUCT WITH THE LARGEST MAXIMUM NORMALIZED RATING (ONE PER LINE)
+        list.forEach(data -> {
+            System.out.println("Product "+data._2() + " maxNormRating " + data._1());
+        });
     }
-
-
 }
