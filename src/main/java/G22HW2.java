951952b3e5c13cc4b41651bb468aaa77063cc79c
Jokie <zanatta.gabriele97@gmail.com>
2021-05-05 18:16:56 +0200

    Point 4 done, point 5 to watch

--- src/main/java/G22HW2.java
+++ src/main/java/G22HW2.java
@@ -1,7 +1,6 @@
-import com.twitter.chill.Tuple1LongSerializer;
+
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.broadcast.Broadcast;
 import org.apache.spark.mllib.linalg.Vector;
@@ -49,28 +48,25 @@ public class G22HW2 {
         //number of partitions
         int p = 8;
 
-
-        Random r = new Random();
-
         Broadcast<ArrayList<Tuple2<Integer, Long>>> sharedClusterSizes = sc.broadcast(new ArrayList<Tuple2<Integer, Long>>());
         Broadcast<List<Tuple2<Vector, Integer>>> clusteringSample;
 
-        float exactSilhSample = 0;
-        float approxSilhFull = 0;
+        Double exactSilhSample = 0d;
+        Double approxSilhFull = 0d;
+        Long numberOfPoints;
 
         // Subdivide RDD into p random partitions
         JavaPairRDD<Vector, Integer> data = fullClustering.repartition(p).cache();
 
         //SAVE CLUSTERS SIZE
         data.values().countByValue().forEach((key,value) -> sharedClusterSizes.value().add(new Tuple2<>(key, value)));
-        sharedClusterSizes.value().forEach(element ->{
-            System.out.println(element);
-        });
+
 
         //EXTRACT SAMPLE OF THE INPUT CLUSTERING
         temp = fullClustering.flatMapToPair(element -> {
             ArrayList<Tuple2<Vector, Integer>> array = new ArrayList();
             Long size = sharedClusterSizes.value().get(element._2())._2();
+            Random r = new Random();
             Double f = r.nextDouble();
 
             if( f < ((double)t/size))
@@ -80,82 +76,93 @@ public class G22HW2 {
         });
         clusteringSample = sc.broadcast(temp.collect());
 
-        System.out.println("Full sample size = "+clusteringSample.getValue().size()+" \t Max sample size = "+t*k);
 
-
-        JavaPairRDD<Integer, Vector> temp2 = fullClustering.flatMapToPair( element -> {
+        JavaPairRDD<Integer, Tuple2<Integer, Vector>> temp2 = fullClustering.flatMapToPair( element -> {
             ArrayList<Tuple2<Integer, Vector>> array = new ArrayList();
             array.add(new Tuple2<>(element._2(), element._1()));
             return array.iterator();
         })
         .groupByKey()
         .flatMapToPair(element -> {
-            ArrayList<Tuple2<Integer, Vector>> array = new ArrayList();
+            ArrayList<Tuple2<Integer, Tuple2<Integer, Vector>>> array = new ArrayList();
             Long size = sharedClusterSizes.value().get(element._1())._2();
             Float ti = Float.min(size, t);
+            Random r = new Random();
+
 
             if( ti == Float.parseFloat(size.toString())) {
                 for (Vector e : element._2()) {
-                    array.add(new Tuple2<>(element._1(), e));
+                    array.add(new Tuple2<>(0, new Tuple2<>(element._1(), e)));
                 }
             }else{
                 for (Vector e : element._2()) {
                     Float f = r.nextFloat();
                     if (f < (ti / size)) {
-                        array.add(new Tuple2<>(element._1(), e));
+                        array.add(new Tuple2<>(0, new Tuple2<>(element._1(), e)));
                     }
                 }
             }
             return array.iterator();
         });
 
-
         //APPROXIMATE AVERAGE SILHOUETTE COEFFICIENT
         long startA = System.currentTimeMillis();
         //code
-       /* approxParamR1 = clusteringSample
-                //R1 Map Phase: compute sum for ap and bp for each point
-                .flatMapValues((element) -> {
-                    ArrayList<Tuple2<Vector, Tuple2<Float, Float>>> pairs = new ArrayList<>(); //pairs (point, sum_ap, sum_bp )
-                    Iterator<Tuple2<Vector, Integer>> p = element.iterator();
-                    float sumA = 0;
-                    float sumB= 0;
-                    while (p.hasNext())
-                    {
-                        Iterator<Tuple2<Vector, Integer>> d = element.iterator();
-                        while (d.hasNext()) {
-                            if (p.next()._2().equals(d.next()._2()))
-                                sumA = (float) (sumA + Vectors.sqdist(p.next()._1(), d.next()._1()));
-                            //manca sumB che è il minimo della somma delle distanze tra p e tutti gli altri punti per ogni cluster
-                        }
-                        pairs.add(new Tuple2<Vector, Tuple2<Float, Float>>(p.next()._1(), sumA, sumB));
-                    }
-                    return pairs.iterator();
-                });
-                //R1 Reduce: compute ap and bp
-                .flatMapValues((coef) -> {
-                    //ap= 1/|Ci| * |Ci|/t * sum(vectors.sqdist(p,ti))   for each ti in sample for Ci
-                    //bp= min ( 1/|Cj| * |Cj|/t * sum(vectors.sqdist(p,ti)) )  for each ti in sample for Cj different to Ci
+        JavaPairRDD<Integer, Double> temp3 = temp2.groupByKey()
+        .flatMapToPair(element->{
+            ArrayList<Tuple2<Integer, Double>> array = new ArrayList();
+            Double[] sums = new Double[k];
+            Double[] fractions = new Double[k];
+            Double ap;
+            Double bp;
+            Double sp;
+            Integer cluster1;
+            Vector v1;
 
-                 });
+            for(int i = 0; i < k; i++)
+                fractions[i] = 1/Double.min(t, sharedClusterSizes.value().get(i)._2());
 
-        approxParamR2 = approxParamR1
-                //R2 Map: empty
-                //R2 Reduce: for each point compute sp
-                .flatMapValues((sil) -> {
-                    float approxSil = 0;
-                    if (ap>bp)
-                        approxSil = (bp-ap) / ap;
-                    else
-                        approxSil = (bp-ap)/ bp;
+            for(Tuple2<Integer, Vector> tuple1 : element._2()){
+                cluster1 = tuple1._1();
+                v1 = tuple1._2();
+                for(int i = 0; i<k; i++)
+                    sums[i] = 0d;
+                for(Tuple2<Integer, Vector> tuple2 : element._2()){
+                    Integer cluster2 = tuple2._1();
+                    Vector v2 = tuple2._2();
+                    sums[cluster2] = sums[cluster2] + Vectors.sqdist(v1,v2);
+                }
+                for(int i = 0; i<k; i++)
+                    sums[i] = sums[i]*fractions[i];
 
-                    pairs.add(new Tuple2<>(point, approxSil));
-                });
+                ap = sums[cluster1];
+                if(cluster1 == 0)
+                    bp = sums[1];
+                else
+                    bp = sums[0];
 
+                for(int i = 0;i < k; i++){
+                    if(i != cluster1 && bp > sums[i])
+                        bp = sums[i];
+                }
 
-                //compute total approx Silhouette
-                approxSilhFull = sumOfSilh/(t*k);
-    */
+                sp = (bp - ap)/Double.max(ap,bp);
+
+                array.add(new Tuple2<>(0, sp));
+            }
+
+            return array.iterator();
+        });
+
+        numberOfPoints = temp3.count();
+        List<Tuple2<Integer,Double>> list = temp3.collect();
+        Iterator<Tuple2<Integer, Double>> iter = list.iterator();
+        while(iter.hasNext()){
+            Double d = iter.next()._2();
+            approxSilhFull = approxSilhFull + d;
+        }
+
+        approxSilhFull = approxSilhFull/numberOfPoints;
         //end code
         long endA = System.currentTimeMillis();
 
@@ -165,6 +172,53 @@ public class G22HW2 {
         //EXACT AVERAGE SILHOUETTE COEFFICIENT
         long startE = System.currentTimeMillis();
         //code
+        List<Tuple2<Vector, Integer>> fullList = fullClustering.collect();
+        Iterator<Tuple2<Vector, Integer>> fullIter = fullList.iterator();
+
+        Double[] sums = new Double[k];
+        Double[] fractions = new Double[k];
+        Double sp = 0d;
+        for(int i = 0; i < k; i++)
+            fractions[i] = 1/Double.min(t, sharedClusterSizes.value().get(i)._2());
+
+        while(fullIter.hasNext()){
+            Double ap;
+            Double bp;
+            Tuple2<Vector, Integer> tuple1 = fullIter.next();
+            Integer cluster1 = tuple1._2();
+            Vector v1 = tuple1._1();
+            Iterator<Tuple2<Vector, Integer>> fIter = fullList.iterator();
+
+            for(int i = 0; i<k; i++)
+                sums[i] = 0d;
+
+            while(fIter.hasNext()){
+                Tuple2<Vector, Integer> tuple2 = fIter.next();
+                Integer cluster2 = tuple2._2();
+                Vector v2 = tuple2._1();
+                sums[cluster2] = sums[cluster2] + Vectors.sqdist(v1,v2);
+            }
+            for(int i = 0; i<k; i++)
+                sums[i] = sums[i]*fractions[i];
+
+            ap = sums[cluster1];
+            if(cluster1 == 0)
+                bp = sums[1];
+            else
+                bp = sums[0];
+
+            for(int i = 0;i < k; i++){
+                if(i != cluster1 && bp > sums[i])
+                    bp = sums[i];
+            }
+
+            sp = sp + ((bp - ap)/Double.max(ap,bp));
+
+        }
+
+        exactSilhSample = sp/fullList.size();
+
+        //end code
         long endE = System.currentTimeMillis();
 
 
@@ -173,10 +227,10 @@ public class G22HW2 {
         long timeApproxSilhFull = (endA-startA);
 
         // PRINTING THE OUTPUT
-        System.out.println("Value of approxSilhFull:" + approxSilhFull +"\n");
-        System.out.println("Time to compute approxSilhFull:" + timeApproxSilhFull +"\n");
-        System.out.println("Value of exactSilhSample:" + exactSilhSample +"\n");
-        System.out.println("Time to compute exactSilhSample:" + timeExactSilhSample +"\n");
+        System.out.println("Value of approxSilhFull = " + approxSilhFull);
+        System.out.println("Time to compute approxSilhFull = " + timeApproxSilhFull +"ms");
+        System.out.println("Value of exactSilhSample = " + exactSilhSample);
+        System.out.println("Time to compute exactSilhSample = " + timeExactSilhSample +"ms");
     }
 
     public static Tuple2<Vector, Integer> strToTuple (String str){
